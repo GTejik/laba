e0027d8904f6c117d5d6153dfa97fec28d824035
ngalayko <ngalayko@gmail.com>
2016-01-21 22:51:59 +0300

    first laba

---
+++ main.py
@@ -0,0 +1,57 @@
+def read_matrix():
+	#read matrix
+	matrix = {}
+
+	#reading
+	f = open('input.txt', 'r')
+
+	#set of chars
+	matrix['chars'] = f.readline().replace(' ', '')
+	#first state
+	matrix['fstate'] = None
+	#matrix
+	for line in f:
+		#matrix.append(line.split())
+		a = line.split()
+		matrix[a[0]] = a[1:]
+		#remember first state
+		if not matrix['fstate']:
+			matrix['fstate'] = a[0]
+
+	f.close()
+
+	return matrix
+
+#function to check string according to matrix
+def check(string, matrix):
+	state = matrix['fstate']
+	chars = matrix['chars']
+	for c in string:
+		#if forbidden symbols, return false
+		if c not in chars:
+			return False
+		state = matrix[state][chars.find(c)]
+	#if it is final state - return true
+	if matrix[state][-1] == '1':
+		return True
+	return False
+
+def rightGramma(matrix):
+	result = ''
+	chars = matrix['chars']
+	a = list(matrix.keys())
+	a.remove('fstate')
+	a.remove('chars')
+	a.sort()
+	for key in a:
+		result += key + '::='
+		for i in range(len(chars) - 1):
+			result += chars[i] + matrix[key][i] + '|'
+		if matrix[key][-1] == '1':
+			result += 'e'
+		result += '\n'
+	return result
+
+#print(rightGramma(read_matrix()))
+#print('Input string to check')
+#print(check(input(), read_matrix()))
---
+++ run.py
@@ -0,0 +1,23 @@
+import telebot
+import main
+
+TOKEN = '126422692:AAH6cZnU66YKPa8cFtqQmsw6kuIn_Nm9t74'
+bot = telebot.TeleBot(TOKEN, True)
+
+@bot.message_handler(commands = ['setMatrix'])
+def set_matrix(message):
+	matrix = message.text[message.text.find(' ') + 1:]
+	f = open('input.txt', 'w')
+	f.write(matrix)
+	bot.send_message(message.chat.id, 'hey')
+
+@bot.message_handler(commands = ['check'])
+def check_string(message):
+	string = message.text[message.text.find(' ') + 1:]
+	bot.send_message(message.chat.id, main.check(string, main.read_matrix()))
+
+@bot.message_handler(commands = ['rightGramma'])
+def right_gramma(message):
+	bot.send_message(message.chat.id, main.rightGramma(main.read_matrix()))
+
+bot.polling()
