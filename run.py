0a63589acdfa67966dd4fc98291c90c86d1febfc
ngalayko <ngalayko@gmail.com>
2016-01-22 08:33:48 +0300

    new commands

--- run.py
+++ run.py
@@ -4,7 +4,7 @@ import main
 TOKEN = '126422692:AAH6cZnU66YKPa8cFtqQmsw6kuIn_Nm9t74'
 bot = telebot.TeleBot(TOKEN, True)
 
-@bot.message_handler(commands = ['setMatrix'])
+@bot.message_handler(commands = ['set_matrix'])
 def set_matrix(message):
 	matrix = message.text[message.text.find(' ') + 1:]
 	f = open('input.txt', 'w')
@@ -16,11 +16,11 @@ def check_string(message):
 	string = message.text[message.text.find(' ') + 1:]
 	bot.send_message(message.chat.id, main.check(string, main.read_matrix()))
 
-@bot.message_handler(commands = ['rightGramma'])
+@bot.message_handler(commands = ['right_gramma'])
 def right_gramma(message):
 	bot.send_message(message.chat.id, main.rightGramma(main.read_matrix()))
 
-@bot.message_handler(commands = ['getMatrix'])
+@bot.message_handler(commands = ['get_matrix'])
 def show_matrix(message):
 	f = open('input.txt', 'r')
 	bot.send_message(message.chat.id, f.read())
