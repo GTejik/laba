4d318aaecfd70c980d3f6b9954fc064ea6f61bee
ngalayko <ngalayko@gmail.com>
2016-01-27 18:05:26 +0300

    laba 2 started

--- main.py
+++ main.py
@@ -1,50 +1,200 @@
-def read_matrix():
-	#read matrix
+rules = 'Правила для ввода:\nПервая строка - алфавит\nДалее строки состояний в виде:\nсостояние-переходы-финальное?(1 или 0)\nПереходы разделяются пробелами\nЕсли переходов из одного состояния несколько, они разделяются запятой\nНачальное состояние помечается \'-\' в начале строки'
+
+def simplify(regexp, chars):
+	#print(regexp, chars)
+	flag = False
+	for c in regexp:		
+		if chars == [] or c == '|' and not flag:
+			chars.append([])
+		if c == '|' and not flag:
+			continue
+		if c == '(' and not flag:
+			chars[-1].append(c)
+			flag = True
+		elif c == ')' and flag:
+			chars[-1][-1] += c
+			flag = False
+		elif flag:
+			chars[-1][-1] += c
+		elif c == '+' or c == '*':
+			chars[-1][-1] += c
+		else:
+			chars[-1].append(c)
+
+def regexp_to_nka(regexp):
+	chars = []
+
+	simplify(regexp, chars)
+
 	matrix = {}
-	#reading
-	f = open('input.txt', 'r')
-	#set of chars
-	matrix['chars'] = f.readline().replace(' ', '')
-	#first state
-	matrix['fstate'] = None
-	#matrix
-	for line in f:
-		#matrix.append(line.split())
-		a = line.split()
-		matrix[a[0]] = a[1:]
-		#remember first state
-		if not matrix['fstate']:
-			matrix['fstate'] = a[0]
-	f.close()
-	return matrix
+	matrix['s'] = ['0']
+	matrix['e'] = ['0']
+	matrix['z'] = ['1']
 
-#function to check string according to matrix
+	print(chars)
+
+#read matrix from input.txt
+def read_matrix():
+	try:
+		#read matrix
+		matrix = {}
+		#reading
+		f = open('input.txt', 'r')
+		#set of chars
+		matrix['chars'] = f.readline().replace(' ', '').replace('\n', '')
+		#first state
+		matrix['fstate'] = ''
+		#matrix
+		for line in f:
+			#matrix.append(line.split())
+			a = line.split()
+			#remember if first state
+			if a[0][0] == '-':
+				matrix['fstate'] += a[0][1:] + ','
+				a[0] = a[0][1:]
+			#remember row
+			matrix[a[0]] = a[1:]
+		#delete extra ','
+		matrix['fstate'] = matrix['fstate'][:-1]
+		f.close()
+		return matrix
+	except:
+		return 'Не могу считать матриу, проверьте правильность ввода\n\n' + rules
+
+#check string according to determinated state machine
+def dka(string, matrix):
+	try:
+		state = matrix['fstate']
+		chars = matrix['chars']
+		for c in string:
+			#if forbidden symbols, return false
+			if c not in chars:
+				return False
+			state = matrix[state][chars.find(c)]
+		#if it is final state - return true
+		if matrix[state][-1] == '1':
+			return True
+		return False
+	except:
+		return 'Не могу проверить строку по детерминированной матрице, проверьете правильность ввода\n\n' + rules
+
+#transform nka to dka
+def nka_to_dka(matrix):
+	try:
+		result = {}
+
+		chars = matrix['chars']
+		fstate = matrix['fstate'].replace(',', '')
+
+		result[fstate] = []
+
+		#empty states
+		queue = list(result.keys())
+		while queue != []:
+			for states in queue:
+				for state in states:
+					for char in chars:
+						if len(result[states]) > chars.find(char) and matrix[state][chars.find(char)].replace(',', '') not in result[states][chars.find(char)]:
+							result[states][chars.find(char)] += matrix[state][chars.find(char)].replace(',', '')
+						elif len(result[states]) <= chars.find(char):
+							result[states].append(matrix[state][chars.find(char)].replace(',', ''))
+						#deleting repetitions and 'e' in state if it is not equals 'e'
+						if result[states][chars.find(char)] != 'e':
+							ns = ''
+							for c in result[states][chars.find(char)].replace('e', ''):
+								if c not in ns:
+									ns += c
+							result[states][chars.find(char)] = ''.join(sorted(ns))
+				#removing done state
+				queue.remove(states)
+			#adding new states to queue
+			for next_state in result[states]:
+				if next_state not in result.keys():
+					result[next_state] = []
+					queue.append(next_state)
+
+		#mark final states
+		for state, values in result.items():
+			for old_state, old_values in matrix.items():
+				if old_values[-1] == '1' and old_state != 'chars' and old_state in state:
+					if values[-1] != '1' and values[-1] != '0':
+						values.append('1')
+					else:
+						values[-1] = '1'
+				else:
+					if values[-1] != '1' and values[-1] != '0':
+						values.append('0')
+					
+		result['chars'] = chars
+		result['fstate'] = fstate
+
+		return result
+	except:
+		return 'не могу перевести матрицу к детерминированной, проверьте правильность ввода\n\n' + rules
+
+#write matrix to file
+def write_matrix(result):
+	try:
+		#write to file
+		buff = [[c for c in result['chars']]]
+		buff[-1] += ['\n']
+		states = [result['fstate']]
+		for state in result.keys():
+			if state not in states and state != 'chars' and state != 'fstate':
+				states.append(state)
+		for state in states:
+			buff.append([state])
+			buff[-1] += result[state] + ['\n']
+			if state == result['fstate']:
+				buff[-1][0] = '-' + buff[-1][0]
+		buff[-1].pop()
+
+		f = open('input.txt', 'w')
+		string = ''
+		for arr in buff:
+			f.write(' '.join(arr))
+			string += ' '.join(arr)
+		f.close()
+	except:
+		pass
+
+#function to check if it is determinate 
 def check(string, matrix):
-	state = matrix['fstate']
-	chars = matrix['chars']
-	for c in string:
-		#if forbidden symbols, return false
-		if c not in chars:
-			return False
-		state = matrix[state][chars.find(c)]
-	#if it is final state - return true
-	if matrix[state][-1] == '1':
-		return True
-	return False
+	try:
+		if ',' in matrix['fstate']:
+			matrix = nka_to_dka(matrix)
+			write_matrix(matrix)
+			return dka(string, matrix)
 
+		for row in matrix.values():
+			for s in row:
+				if ',' in s:
+					matrix = nka_to_dka(matrix)
+					write_matrix(matrix)
+					return dka(string, matrix)
+	except:
+		return 'Не могу проверить на детерминированность, убедитесь в правильности ввода матрицы\n\n' + rules
+
+	#if it is determinate
+	return dka(string, matrix)
+
+#get right gramma
 def rightGramma(matrix):
-	result = ''
-	chars = matrix['chars']
-	a = list(matrix.keys())
-	a.remove('fstate')
-	a.remove('chars')
-	a.sort()
-	for key in a:
-		result += key + '::='
-		for i in range(len(chars) - 1):
-			result += chars[i] + matrix[key][i] + '|'
-		result = result[:-1]
-		if matrix[key][-1] == '1':
-			result += '|eps'
-		result += '\n'
-	return result
+	try:
+		result = ''
+		chars = matrix['chars']
+		keys = list(matrix.keys())
+		keys.remove('fstate')
+		keys.remove('chars')
+		keys.sort()
+		for key in keys:
+			result += key + '::='
+			for i in range(len(chars)):
+				result += chars[i] + matrix[key][i] + '|'
+			result = result[:-1]
+			if matrix[key][-1] == '1':
+				result += '|eps'
+			result += '\n'
+		return result
+	except:
+		return 'Не могу построить грамматику, убедитесь в правильности ввода матрицы\n\n' + rules
--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import telebot
+from telebot import types
 import main
 
 TOKEN = '126422692:AAH6cZnU66YKPa8cFtqQmsw6kuIn_Nm9t74'
@@ -32,17 +33,53 @@ def show_matrix(message):
 	f = open('input.txt', 'r')
 	bot.send_message(message.chat.id, f.read())
 
-@bot.message_handler(commands = ['laba1_2'])
-def set_matrix_laba2_2(message):
-	f = open('input.txt', 'w')
-	f.write('0 1\nA A B 0\nB B B 1');
-	bot.send_message(message.chat.id, 'проверить, что в последовательности имеется по крайней мере одна \'1\', матрица:\n0 1\nA A B 0\nB B B 1')
+@bot.message_handler(commands = ['regexp'])
+def set_regexp(message):
+	if message.text.find(' ') == -1:
+		bot.send_message(message.chat.id, 'пустой аргумент')
+		return
+	regexp = message.text[message.text.find(' ') + 1:]
 
-@bot.message_handler(commands = ['laba1_3'])
-def set_matrix_laba2_2(message):
+	main.regexp_to_nka(regexp)
+
+	bot.send_message(message.chat.id, regexp)
+
+@bot.message_handler(commands = ['laba'])
+def send_choice(message):
+	markup = types.ReplyKeyboardMarkup()
+	markup.row('Задание 1_2', 'Задание 1_3')
+	markup.row('Недетерминированная', 'Регулярное выражение')
+	bot.send_message(message.chat.id, 'Выберите задание:', reply_markup = markup)
+
+@bot.message_handler(regexp = 'Задание 1')
+def set_matrix_laba2_1(message): 
+	markup = types.ReplyKeyboardHide(selective = False)
 	f = open('input.txt', 'w')
-	f.write('0 1 2 3 4 5 6 7 8 9 + - * / = ,\na d b b b b b b b b b e f e e e e 0\nb b b b b b b b b b b c c c c = a 0\nc g h h h h h h h h h e e e e e a 0\nd e e e e e e e e e e c c c c c a 0\nf e b b b b b b b b b c c c c c a 0\nh h h h h h h h h h h c c c c c a 1\ng g e e e e e e e e e c c c c c a 1\ne e e e e e e e e e e e e e e e a 0');
-	bot.send_message(message.chat.id, 'цепочка состоит из арифметических выражений и не содержит скобок, матрица:\n0 1 2 3 4 5 6 7 8 9 + - * / = ,\na d b b b b b b b b b e f e e e e 0\nb b b b b b b b b b b c c c c = a 0\nc g h h h h h h h h h e e e e e a 0\nd e e e e e e e e e e c c c c c a 0\nf e b b b b b b b b b c c c c c a 0\nh h h h h h h h h h h c c c c c a 1\ng g e e e e e e e e e c c c c c a 1\ne e e e e e e e e e e e e e e e a 0\n\na - начальное состояние\nb - только цифры\nc - арифметический знак встретили\nd - ноль (не может быть в начале) \nf - минус (не может быть -0, но -3 ок)\nh - не только цифры, есть выражения\ng - не только ноль, есть выражения\ne - ошибка\n')
+	f.write('0 1\n-A A B 0\nB B B 1');
+	bot.send_message(message.chat.id, 'проверить, что в последовательности имеется по крайней мере одна \'1\', матрица:\n0 1\n-A A B 0\nB B B 1', reply_markup=markup)
+
+@bot.message_handler(regexp = 'Задание 2')
+def set_matrix_laba2_2(message):
+	markup = types.ReplyKeyboardHide(selective = False)
+	f = open('input.txt', 'w')
+	f.write('0 1 2 3 4 5 6 7 8 9 + - * / = ,\n-a d b b b b b b b b b e f e e e e 0\nb b b b b b b b b b b c c c c = a 0\nc g h h h h h h h h h e e e e e a 0\nd e e e e e e e e e e c c c c c a 0\nf e b b b b b b b b b c c c c c a 0\nh h h h h h h h h h h c c c c c a 1\ng g e e e e e e e e e c c c c c a 1\ne e e e e e e e e e e e e e e e a 0');
+	bot.send_message(message.chat.id, 'цепочка состоит из арифметических выражений и не содержит скобок, матрица:\n0 1 2 3 4 5 6 7 8 9 + - * / = ,\n-a d b b b b b b b b b e f e e e e 0\nb b b b b b b b b b b c c c c = a 0\nc g h h h h h h h h h e e e e e a 0\nd e e e e e e e e e e c c c c c a 0\nf e b b b b b b b b b c c c c c a 0\nh h h h h h h h h h h c c c c c a 1\ng g e e e e e e e e e c c c c c a 1\ne e e e e e e e e e e e e e e e a 0\n\na - начальное состояние\nb - только цифры\nc - арифметический знак встретили\nd - ноль (не может быть в начале) \nf - минус (не может быть -0, но -3 ок)\nh - не только цифры, есть выражения\ng - не только ноль, есть выражения\ne - ошибка\n', reply_markup=markup)
+
+@bot.message_handler(regexp = 'Недетерминированная')
+def set_nd_matrix(message):
+	markup = types.ReplyKeyboardHide(selective = False)
+	f = open('input.txt', 'w')
+	f.write('0 1\n-a a,b c 0\n-b e c 1\nc e a,c 1\ne e e 0');
+	bot.send_message(message.chat.id, 'матрица:\n0 1\n-a a,b c 0\n-b e c 1\nc e a,c 1\ne e e 0', reply_markup=markup)
+
+@bot.message_handler(regexp = 'Регулярное выражение')
+def set_l_regexp(message):
+	markup = types.ReplyKeyboardHide(selective = False)
+	regexp = '1+|1*01(11|a)+'
+
+	bot.send_message(message.chat.id, 'Установлено: '+ regexp, reply_markup=markup)
+
+	main.regexp_to_nka(regexp)
 
 @bot.message_handler(commands = ['help'])
 def send_help(message):
